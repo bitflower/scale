f78d08c74ba4a5da4ae4a3384130312e59b2773a
Rómulo Lazarde <romulo.lazarde@iconstorm.com>
2020-05-12 07:05:29 +0200

    feat: adds accordion component base class and styles

---
+++ packages/components/src/components/accordion/accordion.styles.ts
@@ -0,0 +1,15 @@
+import { JssStyle } from 'jss';
+
+export const styles: JssStyle = {
+  accordion: {
+    boxSizing: 'border-box',
+    background: '#eee',
+    color: '#333',
+    width: '100%',
+    padding: '1rem',
+    textAlign: 'left',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'space-between',
+  }
+};
---
+++ packages/components/src/components/accordion/accordion.tsx
@@ -0,0 +1,49 @@
+import { Component, Prop, h, Method } from '@stencil/core';
+import { CssClassMap } from '../../utils/utils';
+import classNames from 'classnames';
+
+import { styles } from './accordion.styles';
+import { CssInJs } from '../../utils/css-in-js';
+import { StyleSheet } from 'jss';
+import Base from '../../utils/base-interface';
+
+@Component({
+  tag: 'scale-accordion',
+  shadow: false,
+})
+
+export class Accordion implements Base {
+  /** (optional) Custom class */
+  @Prop() customClass?: string = '';
+
+  /** (optional) Injected jss styles */
+  @Prop() styles?: StyleSheet;
+
+  /** decorator Jss stylesheet */
+  @CssInJs('Accordion', styles) stylesheet: StyleSheet;
+
+  componentWillLoad() {}
+  componentWillUpdate() {}
+  componentDidUnload() {}
+
+
+  render() {
+    // const { classes } = this.stylesheet;
+
+    return (
+      
+        <div class={this.getCssClassMap()}>
+
+        </div>
+      
+    );
+  }
+
+  getCssClassMap(): CssClassMap {
+    const { classes } = this.stylesheet;
+    return classNames(
+      classes.alert,
+      this.customClass && this.customClass
+    );
+  }
+}
